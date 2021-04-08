fe22fe78375465a9fc8a0a0874c39e6bf51d3096
alexey_gamov <4319149+alexey-gamov@users.noreply.github.com>
2021-04-09 09:49:22 +1100

    Interactive save button
    Disabled button state for last save action

--- options.js
+++ options.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+	var save = document.getElementsByName('save')[0];
+
 	chrome.storage.sync.get({
 		ip: '192.168.1.0',
 		login: 'admin',
@@ -11,15 +13,24 @@ document.addEventListener('DOMContentLoaded', function () {
 		document.getElementsByName('pass')[0].value = items.pass;
 		document.getElementsByName('number')[0].value = items.number;
 		document.getElementsByName('pickup')[0].value = items.pickup;
+		save.disabled = true;
 	});
 
-	document.getElementsByName('save')[0].addEventListener('click', function() {
+	save.addEventListener('click', function() {
 		chrome.storage.sync.set({
 			ip: document.getElementsByName('ip')[0].value,
 			login: document.getElementsByName('login')[0].value,
 			pass: document.getElementsByName('pass')[0].value,
 			number: document.getElementsByName('number')[0].value,
 			pickup: document.getElementsByName('pickup')[0].value
+		}, function(items) {
+			save.disabled = true;
 		});
     });
+
+	document.querySelectorAll('input').forEach(function (item) {
+		item.addEventListener('focus', function (event) {
+			save.disabled = false;
+		});
+	});
 });
