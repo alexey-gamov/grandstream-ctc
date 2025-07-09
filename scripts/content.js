0fc993ae262ff4ec771d0c47e8545359cf723eee
Гамов Алексей Сергеевич <atomos@mail.ru>
2025-07-09 12:39:13 +1100

    Handle all type of call URLs
    Works wiith active links even if auto-detect number feature is disabled

--- scripts/content.js
+++ scripts/content.js
@@ -2,52 +2,64 @@ window.onload = () => {
 	const platform = chrome || browser;
 	let settings = ['confirm', 'content'];
 
-    platform.storage.local.get(settings, (items) => {
+	platform.storage.local.get(settings, (items) => {
 		if (items.content == 0) return
 		else settings.confirm = items.confirm;
 
         const filter = (inspect) => {
-            if (inspect.parentNode.nodeName === 'SCRIPT') {
-                return NodeFilter.FILTER_REJECT;
-            }
+			if (inspect.parentNode.nodeName === 'SCRIPT') {
+				return NodeFilter.FILTER_REJECT;
+			}
 
-            let found = false;
+			let found = false;
 
-            if (inspect.textContent) {
-                const regex = /^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){6,})(?:[\-\.\ \\\/]?)$/igm;
-                found = inspect.textContent.match(regex);
-            }
+			if (inspect.textContent) {
+				const regex = /^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){6,})(?:[\-\.\ \\\/]?)$/igm;
+				found = inspect.textContent.match(regex);
+			}
 
-            if ('tel' in inspect.parentNode.dataset) {
-                found = (inspect.parentNode.dataset.tel === 'true');
-            }
+			if ('tel' in inspect.parentNode.dataset) {
+				found = (inspect.parentNode.dataset.tel === 'true');
+			}
 
-            return found ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP;
-        };
+			return found ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP;
+		};
 
-        const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, filter, false);
-        const nodes = [];
+		const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, filter, false);
+		const nodes = [];
 
-        let node;
+		let node;
 
-        while (node = walker.nextNode()) {
-            nodes.push(node);
-        }
+		while (node = walker.nextNode()) {
+			nodes.push(node);
+		}
 
-        nodes.forEach((node) => {
-            const number = node.textContent.replace(/[^0-9]/gi, '');
-            const object = document.createElement('a');
+		nodes.forEach((node) => {
+			const number = node.textContent.replace(/[^0-9]/gi, '');
+			const object = document.createElement('a');
 
-            object.href = 'phone:' + number;
-            object.innerHTML = node.textContent;
+			object.style.color = 'inherit';
+			object.style.textDecoration = 'inherit';
 
-            object.onclick = () => {
-                if (Boolean(Number(settings.confirm)) && !confirm(platform.i18n.getMessage('confirmation', number))) return;
-                platform.runtime.sendMessage({action: 'call', data: number});
-            };
+			object.href = 'phone:' + number;
+			object.innerHTML = node.textContent;
 
-            node.replaceWith(object);
-        });
+			node.replaceWith(object);
+		});
+
+		document.addEventListener('click', (event) => {
+			if (event.target.tagName.toLowerCase() !== 'a') return;
+
+			let [protocol, number] = event.target.getAttribute('href').split(':');
+
+			if (['tel', 'callto', 'sip', 'phone'].includes(protocol)) {
+				number = number.replace(/[^0-9]/gi, '');
+				event.preventDefault();
+
+				if (Boolean(Number(settings.confirm)) && !confirm(platform.i18n.getMessage('confirmation', number))) return;
+				platform.runtime.sendMessage({action: 'call', data: number});
+			}
+		});
     });
 
 	platform.storage.onChanged.addListener((changes, area) => {
